9fd164c06718536f9a7e3fe8168264837bf8433d
Lokeshwaran <lokeshwaran411@gmail.com>
2019-01-04 19:02:05 +0530

    4-1-2019
    javascript dynamic injection to dom

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,8 @@
-
+let bimages=["url(../myfirstwebsite/images/image1.jpg)","url(../myfirstwebsite/images/image2.jpg)","url(../myfirstwebsite/images/image3.jpg)"];
+let slidernav = document.querySelector('.slider-nav1');
+console.log(`slidernav: ${slidernav}`);
 function changelabel(n){
-  var sample=document.getElementsByTagName("input");
+  let sample=document.getElementsByTagName("input");
   if(n==1)
   {
     sample[0].setAttribute("placeholder","Search By City or Address:");
@@ -12,6 +14,7 @@ sample[0].setAttribute("placeholder","Search By Address or City:");
 sample[0].setAttribute("placeholder","Search By City or Address:");
     }
   }
+  /*
 window.onscroll=function(){
   myfunction()
 };
@@ -19,42 +22,65 @@ window.onscroll=function(){
 
 function myfunction(){
   console.log("abc");
-  var navbar=document.querySelector('.top-bottom');
-  var sticky=navbar.offsetTop;
+  let navbar=document.querySelector('.top-bottom');
+  let sticky=navbar.offsetTop;
   if (window.pageYOffset >= sticky) {
     // navbar.classList.add("sticky");
   } else {
     // navbar.classList.remove("sticky");
   }
 }
-function changebackground(n){
-  let sl = ['images/image1.jpg', 'url(images/image2.jpg)']
-  console.log("slider");
-  var slider=document.querySelector('.slider');
-  var img="image"+n+".jpg";
-  console.log("image",img);
-  var sp1=document.querySelector('#sp1');
-  var sp2=document.querySelector('#sp2');
-  var sp3=document.querySelector('#sp3');
-  if(n==1){
-    slider.style.setProperty("background-image","url(images/image1.jpg)");
-    slider.style.setProperty("transition","0.5s width");
-    sp1.style.setProperty("background-color","#ffffff");
-    sp2.style.setProperty("background","transparent");
-    sp3.style.setProperty("background","transparent");
+*/
+// function changebackground(n){
+//   let sl = ['images/image1.jpg', 'url(images/image2.jpg)']
+//   console.log("slider");
+//   let slider=document.querySelector('.slider');
+//   let img="image"+n+".jpg";
+//   console.log("image",img);
+//   let sp1=document.querySelector('#sp1');
+//   let sp2=document.querySelector('#sp2');
+//   let sp3=document.querySelector('#sp3');
+//   if(n==1){
+//     slider.style.setProperty("background-image","url(images/image1.jpg)");
+//     slider.style.setProperty("transition","0.5s width");
+//     sp1.style.setProperty("background-color","#ffffff");
+//     sp2.style.setProperty("background","transparent");
+//     sp3.style.setProperty("background","transparent");
+//   }
+//   else if (n==2) {
+//     slider.style.setProperty("background-image","url(images/image2.jpg)");
+//     slider.style.setProperty("transition","0.5s width");
+//     sp1.style.setProperty("background-color","transparent");
+//     sp2.style.setProperty("background","#ffffff");
+//     sp3.style.setProperty("background","transparent");
+//   }
+//   else {
+//     slider.style.setProperty("background-image","url(images/image3.jpg)");
+//     slider.style.setProperty("transition","0.5s width");
+//     sp1.style.setProperty("background-color","transparent");
+//     sp2.style.setProperty("background","transparent");
+//     sp3.style.setProperty("background","#ffffff");
+//   }
+// }
+//-----------slider-background-changer-----------------
+
+
+for (let i = 0; i < bimages.length; i++) {
+  console.log("background");
+  let radio=document.createElement('span');
+  radio.classList.add("slider-radio");
+  if(slidernav==null) {
+    console.log("NULL");
   }
-  else if (n==2) {
-    slider.style.setProperty("background-image","url(images/image2.jpg)");
-    slider.style.setProperty("transition","0.5s width");
-    sp1.style.setProperty("background-color","transparent");
-    sp2.style.setProperty("background","#ffffff");
-    sp3.style.setProperty("background","transparent");
+  else{
+    slidernav.appendChild(radio);
+    radio.addEventListener("click",function(){
+      radio.style.setProperty("background","#ffffff");
+      changeBackground(i);
+    });
   }
-  else {
-    slider.style.setProperty("background-image","url(images/image3.jpg)");
-    slider.style.setProperty("transition","0.5s width");
-    sp1.style.setProperty("background-color","transparent");
-    sp2.style.setProperty("background","transparent");
-    sp3.style.setProperty("background","#ffffff");
-  }
+}
+function changeBackground(n){
+  let slider=document.querySelector(".slider");
+  slider.style.setProperty("background-image",bimages[n]);
 }
